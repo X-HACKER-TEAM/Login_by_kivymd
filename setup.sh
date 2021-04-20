53ee5bc389858a526e8e21547e7877ac1742d8af
X-HACKER-TEAM <78023331+X-HACKER-TEAM@users.noreply.github.com>
2021-04-20 15:34:07 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+echo -e "\033[1;31mInstalling please wait......\033[0m"
+sleep 1
+v=`which python3`
+v2=`pip list | grep N4Tools`
+
+if [[ $v == *"python3"* ]]; then
+  #pip install --upgrade pip
+  if [[ $v2 == *"N4Tools"* ]]; then
+    echo -e "\033[1;33mAlready installed...\033[0m"
+  else
+    pip install N4Tools
+    pip install requests
+  fi
+else
+  apt-get install python3
+  pip install N4Tools
+  pip install requests
+fi
+clear
+chmod +x scan.py && ./scan.py
